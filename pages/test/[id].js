9d1623447a1408365257b84a073bf1fcc4e92399
mlstubblefield <mlstubblefield@speedwaymotors.com>
2020-06-15 15:05:25 -0400

    Update [id].js

--- pages/test/[id].js
+++ pages/test/[id].js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 export async function getStaticPaths() {
 
-  const ids = [...Array(100000).keys()]
+  const ids = [...Array(10).keys()]
   return {
     paths: ids.map((id) => {
       return {
