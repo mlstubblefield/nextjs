ef59c1f5eab1e2431c6f4346460a0f9e4450c157
mlstubblefield <mlstubblefield@speedwaymotors.com>
2020-09-08 11:42:43 -0400

    Update [id].js

--- pages/test/[id].js
+++ pages/test/[id].js
@@ -30,6 +30,10 @@ export async function getStaticProps({ params }) {
   }
 }
 
+export async function getServerSideProps({ params }) {
+  return getStaticProps(params)
+}
+
 export default function Post({ id }) {
   return (
     <main>
