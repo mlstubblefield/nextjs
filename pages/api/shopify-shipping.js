d56cf5b52997c54621f467df2b04c8244c80abd3
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2021-08-12 12:24:14 -0500

    TNT!

--- pages/api/shopify-shipping.js
+++ pages/api/shopify-shipping.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
               {
                 "currency": "USD",
                 "description": "This is the fastest option by far",
-                "max_delivery_date": "2022-04-12 14:48:45 -0400",
+                "max_delivery_date": "2024-04-12 14:48:45 -0400",
                 "min_delivery_date": "2022-04-12 14:48:45 -0400",
                 "service_code": "ON",
                 "service_name": "Yolo",
@@ -17,7 +17,7 @@ export default function handler(req, res) {
               },
               {
                 "currency": "USD",
-                "max_delivery_date": "2022-04-22 14:48:45 -0400",
+                "max_delivery_date": "2024-04-22 14:48:45 -0400",
                 "min_delivery_date": "2022-04-11 14:48:45 -0400",
                 "service_code": "2D",
                 "service_name": "Yolo Overnight",
@@ -25,7 +25,7 @@ export default function handler(req, res) {
               },
               {
                 "currency": "USD",
-                "max_delivery_date": "2022-04-14 14:48:45 -0400",
+                "max_delivery_date": "2024-04-14 14:48:45 -0400",
                 "min_delivery_date": "2022-04-12 14:48:45 -0400",
                 "service_code": "1D",
                 "service_name": "Yololololo",
