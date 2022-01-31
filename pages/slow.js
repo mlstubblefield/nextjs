c1ea24e734b5c6c8eb1417ddb628af23658211ef
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2022-01-31 12:10:45 -0600

    some more tweaks

--- pages/slow.js
+++ pages/slow.js
@@ -5,13 +5,13 @@ export default function Slow(){
     const renders = useRef(0)
     useEffect(() => {
         const intervalId = setInterval(() => {
-            if(renders.current < 100){
+            if(renders.current < 200){
                 renders.current += 1
                 setState({...Datar})
             }else{
                 clearInterval(intervalId)
             }
-        }, 20) 
+        }, 30) 
     }, [])
 
     return <Thingy {...state} />
